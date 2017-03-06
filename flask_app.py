95f81928c742833ba8d9aa028d17af047a30760a
Robert Perez <robertjasonperez@gmail.com>
2017-03-06 00:13:57 -0600

    Refactoring of intergraph endpoint
    split the main method into two logical parts, generating the randomized
    list minus the person requesting the output, also introduced a get/post
    feature for the random person endpoint

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-from flask import make_response,Flask,jsonify,render_template,request
+from flask import make_response, Flask, jsonify, render_template, request
 import random
 import sqlite3
 import datetime
@@ -9,111 +9,124 @@ from matplotlib.dates import DateFormatter
 from matplotlib.figure import Figure
 from pandas import date_range, Series
 from io import StringIO
-import intergraph
+from intergraph.intergraph import random_member_response
 
 import rasberry_pi.routes
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/random/',methods=['POST'])
+
+@app.route('/random/', methods=['GET', 'POST'])
 def random_route():
-    return intergraph.random_team_member(request)
+    if request.method == 'GET':
+        return random_member_response()
+    return random_member_response(request.form['user_name'], request.form['text'])
+
+
 
 @app.route('/hotels/')
 @app.route('/hotels/<group>/<category>')
 @app.route('/hotels/<group>')
-def show_entries(group=None,category=None):
+def show_entries(group=None, category=None):
     db = sqlite3.connect('hotel.db')
     sql_string = "select * from Hotels"
     data = ()
     if group or category:
         if category:
             sql_string = sql_string + " WHERE hotel_group=? and category=?"
-            data = ((group),(category))
+            data = ((group), (category))
         else:
             sql_string = sql_string + " WHERE hotel_group=?"
             data = ((group),)
-    cur = db.execute(sql_string,(data))
+    cur = db.execute(sql_string, (data))
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
+
 
 @app.route('/north/')
 @app.route('/north/<group>/<category>')
 @app.route('/north/<group>')
-def north(group=None,category=None):
-    return geographic_finder(True,group,category)
+def north(group=None, category=None):
+    return geographic_finder(True, group, category)
+
 
 @app.route('/south/')
 @app.route('/south/<group>/<category>')
 @app.route('/south/<group>')
-def south(group=None,category=None):
-    return geographic_finder(False,group,category)
+def south(group=None, category=None):
+    return geographic_finder(False, group, category)
 
-def geographic_finder(max: bool, group=None,category=None):
+
+def geographic_finder(max: bool, group=None, category=None):
     db = sqlite3.connect('hotel.db')
     type = "max"
     if max == False:
         type = "min"
-    sql_string = "select "+type+"(lat) from Hotels"
+    sql_string = "select " + type + "(lat) from Hotels"
     data = ()
     if group or category:
         if category:
             sql_string = sql_string + " where hotel_group=? and category=?"
-            data = ((group),(category))
+            data = ((group), (category))
         else:
             sql_string = sql_string + " where hotel_group=?"
             data = ((group),)
-    cur = db.execute(sql_string,(data))
+    cur = db.execute(sql_string, (data))
     lat = cur.fetchone()[0]
     print(lat)
-    cur = db.execute("SELECT * from Hotels WHERE lat='"+str(lat)+"'")
+    cur = db.execute("SELECT * from Hotels WHERE lat='" + str(lat) + "'")
     entries = cur.fetchall()
     print((lat))
     return render_template('show_entries.html', entries=entries)
 
+
 @app.route('/hotels/detail/<id>')
-def detail(id:str = '0'):
+def detail(id: str = '0'):
     db = sqlite3.connect('hotel.db')
     sql_string = "select * from Hotels WHERE id=?"
-    cur = db.execute(sql_string,((id),))
+    cur = db.execute(sql_string, ((id),))
     entry = cur.fetchone()
-    #site = requests.get('https://maps.googleapis.com/maps/api/staticmap?center='+str(entry[3])+','+str(entry[4])+'&zoom=12&size=400x400&key=AIzaSyDXz9d7haybjrPP6iL2V0yknNb-aKwyK8w')
+    # site = requests.get('https://maps.googleapis.com/maps/api/staticmap?center='+str(entry[3])+','+str(entry[4])+'&zoom=12&size=400x400&key=AIzaSyDXz9d7haybjrPP6iL2V0yknNb-aKwyK8w')
 
-    return render_template('hotel_detail.html',entry = entry)
+    return render_template('hotel_detail.html', entry=entry)
+
 
 @app.route('/temperature/<humidity>/<temperature>')
-def templog(humidity=None,temperature=None):
+def templog(humidity=None, temperature=None):
     db = sqlite3.connect('temp.db')
     c = db.cursor()
     sql_string = "insert into TempData (temperature, humidity, date) values (?,?,?)"
-    data = ((temperature),(humidity), (datetime.datetime.now()))
-    c.execute(sql_string,data)
+    data = ((temperature), (humidity), (datetime.datetime.now()))
+    c.execute(sql_string, data)
     db.commit()
     db.close()
     return ""
 
+
 def smoother(input):
     output = []
-    for x in range(0,len(input)):
-        slice_array = input[x-2:x+3]
+    for x in range(0, len(input)):
+        slice_array = input[x - 2:x + 3]
         for item in slice_array:
             if item == 0:
                 item = input[x]
         average = sum(slice_array)
-        average = average/5
+        average = average / 5
         if len(slice_array) == 5:
             output.append(average)
     return output
+
 
 @app.route('/tempMap/')
 def tempmap():
     db = sqlite3.connect('temp.db')
     c = db.cursor()
-    sql_string = "select * from TempData"#" WHERE date > '" + str(datetime.datetime.now() - datetime.timedelta(hours=24))+"'"
+    sql_string = "select * from TempData"  # " WHERE date > '" + str(datetime.datetime.now() - datetime.timedelta(hours=24))+"'"
     data = c.execute(sql_string)
     db.commit()
     entries = data.fetchall()
@@ -124,8 +137,8 @@ def tempmap():
         temps.append(x[1])
         hum.append(x[2])
         time.append(x[3])
-    ts = Series(smoother(temps),index=time)
-    hs = Series(smoother(hum),index=time)
+    ts = Series(smoother(temps), index=time)
+    hs = Series(smoother(hum), index=time)
     hs.plot()
     fig = ts.plot().get_figure()
     canvas = FigureCanvas(fig)
@@ -135,14 +148,16 @@ def tempmap():
     response.headers['Content-Type'] = 'image/png'
     return response
 
+
 @app.route('/tempMap2/')
 @app.route('/tempMap2/hours=<hours>')
-def tempmap2(hours = None):
+def tempmap2(hours=None):
     db = sqlite3.connect('temp.db')
     c = db.cursor()
     sql_string = "select * from TempData"
     if hours != None:
-        sql_string = sql_string + " WHERE date > '" + str(datetime.datetime.now() - datetime.timedelta(hours=int(hours)))+"'"
+        sql_string = sql_string + " WHERE date > '" + str(
+            datetime.datetime.now() - datetime.timedelta(hours=int(hours))) + "'"
     data = c.execute(sql_string)
     db.commit()
     entries = data.fetchall()
@@ -152,12 +167,12 @@ def tempmap2(hours = None):
     for x in entries:
         temps.append(x[1])
         hum.append(x[2])
-        time.append(datetime.datetime.strptime(x[3],"%Y-%m-%d %H:%M:%S.%f") - datetime.timedelta(hours = 6))
-    fig=Figure()
+        time.append(datetime.datetime.strptime(x[3], "%Y-%m-%d %H:%M:%S.%f") - datetime.timedelta(hours=6))
+    fig = Figure()
     ax = fig.add_subplot(111)
     t, = ax.plot_date(time, smoother(temps), '-', label="t")
-    h, = ax.plot_date(time, smoother(hum), '-', label = "h")
-    ax.legend([t,h],["Temperature (C)","Humidity (0-100%)"],loc=3)
+    h, = ax.plot_date(time, smoother(hum), '-', label="h")
+    ax.legend([t, h], ["Temperature (C)", "Humidity (0-100%)"], loc=3)
     ax.xaxis.set_major_formatter(DateFormatter('%m-%d %I:%M'))
     fig.autofmt_xdate()
     canvas = FigureCanvas(fig)
@@ -172,9 +187,10 @@ def tempmap2(hours = None):
 def temp_data():
     return rasberry_pi.routes.temp_data()
 
+
 @app.route('/tempMap3/')
 @app.route('/tempMap3/hours=<hours>')
-def temptesting(hours = None):
+def temptesting(hours=None):
     import plotly
     import json
     import pandas as pd
@@ -186,7 +202,8 @@ def temptesting(hours = None):
     c = db.cursor()
     sql_string = "select * from TempData"
     if hours != None:
-        sql_string = sql_string + " WHERE date > '" + str(datetime.datetime.now() - datetime.timedelta(hours=int(hours)))+"'"
+        sql_string = sql_string + " WHERE date > '" + str(
+            datetime.datetime.now() - datetime.timedelta(hours=int(hours))) + "'"
     data = c.execute(sql_string)
     db.commit()
     entries = data.fetchall()
@@ -239,8 +256,5 @@ def temptesting(hours = None):
                            graphJSON=graphJSON)
 
 
-
 if __name__ == '__main__':
     app.run()
-
-
--- intergraph/intergraph.py
+++ intergraph/intergraph.py
@@ -1,42 +1,53 @@
 from flask import jsonify
-from random import random
+from random import shuffle,randint
 
 
-def random_member_response(request):
-    data = {"text": rosie(str(random_team_member(request)).replace('[','').replace(']','').replace("'",'').replace(',','')), "response_type": "in_channel","parse":"full","link_names":1}
+def random_member_response(username=None,arguments=None):
+    data = {"text": ''.join(x for x in rosie(str(random_team_member(username,arguments))) if x not in "[],'\""), "response_type": "in_channel","parse":"full","link_names":1}
     resp = jsonify(data)
     return resp
 
 
-def random_team_member(request):
-    members = [":colosi: @colosicm ",":roberto: @perez ", ":nathan: @nwplotts ",":rosa: @wrosa ",":duke: @kevinduke ",":sean: @sean "]
-    if str(request.form['user_name']) == 'perez':
-        members.remove(":roberto: @perez ")
-    if str(request.form['user_name']) == 'colosicm':
-        members.remove(":colosi: @colosicm ")
-    if str(request.form['user_name']) == 'kevinduke':
-        members.remove(":duke: @kevinduke ")
-    if str(request.form['user_name']) == 'nwplotts':
-        members.remove(":nathan: @nwplotts ")
-    if str(request.form['user_name']) == 'sean':
-        members.remove(":sean: @sean ")
-    if str(request.form['user_name']) == 'wrosa':
-        members.remove(":rosa: @wrosa ")
-    random.shuffle(members)
-    if len(str(request.form['text'])) == 0:
+def random_team_member(username,arguments):
+    members = generate_member_list_minus_requester(username)
+    if len(str(arguments)) == 0:
         return members[0]
-    for arguments in str(request.form['text']).split(' '):
+    return remove_requested_members_from_pool(arguments, members)
+
+
+def remove_requested_members_from_pool(arguments, members):
+    for arguments in str(arguments).split(' '):
         for name in members:
             if arguments in name:
                 members.remove(name)
     try:
-        test = int(str(request.form['text']).split(' ')[0])
+        test = int(str(arguments).split(' ')[0])
         return members[:test]
     except Exception as e:
         return members[0]
 
+
+def generate_member_list_minus_requester(username):
+    members = [":colosi: @colosicm ", ":roberto: @perez ", ":nathan: @nwplotts ", ":rosa: @wrosa ",
+               ":duke: @kevinduke ", ":sean: @sean "]
+    if str(username) == 'perez':
+        members.remove(":roberto: @perez ")
+    if str(username) == 'colosicm':
+        members.remove(":colosi: @colosicm ")
+    if str(username) == 'kevinduke':
+        members.remove(":duke: @kevinduke ")
+    if str(username) == 'nwplotts':
+        members.remove(":nathan: @nwplotts ")
+    if str(username) == 'sean':
+        members.remove(":sean: @sean ")
+    if str(username) == 'wrosa':
+        members.remove(":rosa: @wrosa ")
+    shuffle(members)
+    return members
+
+
 def rosie(input: str):
-    choice = random.randint(0,2)
+    choice = randint(0,2)
     if choice == 0:
         input = input.replace(":rosa:",":rosie:")
     if choice == 1:
